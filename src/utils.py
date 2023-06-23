9eb8221961ca3ee50d7444b7a1dff51ed2f21e1d
Pranay Gupta <pranay20228@iiitd.ac.in>
2023-06-23 19:39:04 +0530

    Implemented utility functions
    Added functions for
    - setting up the initial CSV file
    - deleting directories along with their contents

---
+++ src/utils.py
@@ -0,0 +1,49 @@
+import csv
+import os
+
+
+
+def setup_output_csv(output_file_path):
+    
+    #Checking and removing if an older output file exists
+    if os.path.isfile(output_file_path):
+        print('Existing file found.\nRemoving existing file...')
+        os.remove(output_file_path)
+
+    headers = [
+        'Bussiness__City',
+        'Bussiness__Country',
+        'Bussiness__Description',
+        'Bussiness__Name',
+        'Bussiness__StreetAddress',
+        'Bussiness__Zipcode',
+        'Customer__Address__line1',
+        'Customer__Address__line2',
+        'Customer__Email',
+        'Customer__Name',
+        'Customer__PhoneNumber',
+        'Invoice__BillDetails__Name',
+        'Invoice__BillDetails__Quantity',
+        'Invoice__BillDetails__Rate',
+        'Invoice__Description',
+        'Invoice__DueDate',
+        'Invoice__IssueDate',
+        'Invoice__Number',
+        'Invoice__Tax'
+    ]
+
+    with open(output_file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+    print('CSV initialized...')
+
+
+def delete_directory(directory_path):
+    for root, dirs, files in os.walk(directory_path, topdown=False):
+        for file in files:
+            file_path = os.path.join(root, file)
+            os.remove(file_path)
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            os.rmdir(dir_path)
+    os.rmdir(directory_path)
